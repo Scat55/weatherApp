3d1a7087da7aa6ee3607a8a21ca8ec770828c247
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-02-21 17:32:57 +0300

    feat: create main-window component

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
 <template>
-  <div></div>
+  <div class="app">
+    <MainWindowUi />
+  </div>
 </template>
+<style lang="scss" scoped>
+.app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
---
+++ components/main-window/ui.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="main-window"></div>
+</template>
+
+<style scoped lang="scss">
+.main-window {
+  width: 548px;
+  height: 660px;
+  flex-shrink: 0;
+  border-radius: 30px;
+  background: linear-gradient(144deg, rgba(255, 255, 255, 0.11) 0%, rgba(255, 255, 255, 0.22) 100%);
+  filter: blur(0px);
+  backdrop-filter: blur(25px);
+  padding: 20px;
+}
+</style>
