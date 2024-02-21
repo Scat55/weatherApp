37b1fd028fcf0b5e33ace093ea45c2f2b6978866
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-02-21 17:24:00 +0300

    feat: change settings title and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/png', href: '/weatherIcon.png' }],
+      title: 'Погода',
+    },
+  },
   css: ['/assets/styles/index.scss'],
   modules: [
     '@pinia/nuxt',
