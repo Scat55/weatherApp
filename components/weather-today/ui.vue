959bde7b7b8590ae31d7b18002eae536f1e0d1df
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-02-22 15:36:15 +0300

    feat: add information about the weather for today

--- components/weather-today/ui.vue
+++ components/weather-today/ui.vue
@@ -3,14 +3,88 @@ import { useWeatherAPIStore } from '@/stores/getWeatherInfo';
 
 const weatherInfo = useWeatherAPIStore();
 
-console.log(weatherInfo.weatherData);
+const monthNames = [
+  'января',
+  'февраля',
+  'марта',
+  'апреля',
+  'мая',
+  'июня',
+  'июля',
+  'августа',
+  'сентября',
+  'октября',
+  'ноября',
+  'декабря',
+];
+
+const date = new Date();
+
+const dateNow = date.getDate();
+const monthNow = date.getMonth();
+const yearNow = date.getFullYear();
 </script>
 
 <template>
-  <div>
-    <!-- Рендер информации о погоде только тогда, когда weatherInfo не равен null -->
-    {{ weatherInfo.weatherData.name }}
+  <div class="weather">
+    <div class="weather-data">
+      <p class="weather-data__day">
+        Сегодня: {{ dateNow }} {{ monthNames[monthNow] }} {{ yearNow }}
+      </p>
+      <p class="weather-data__nameCity">{{ weatherInfo.weatherData.name }}</p>
+      <span class="weather-data__description">{{
+        weatherInfo.weatherData.weather[0].description
+      }}</span>
+      <div class="weather-data__temp">
+        <span class="temp__info">
+          {{ (weatherInfo.weatherData.main.temp - 273.15).toFixed() }}
+          <sup>o</sup>C
+        </span>
+        <img
+          :src="`https://openweathermap.org/img/wn/${weatherInfo.weatherData.weather[0].icon}@2x.png`"
+          alt="weatherIcon"
+          class="temp__img"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.weather {
+  &-data {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    margin-top: 10px;
+
+    &__day {
+      color: #fff;
+      font-size: 12px;
+      margin-bottom: 10px;
+    }
+    &__nameCity {
+      color: #fff;
+      font-size: 24px;
+    }
+    &__description {
+      color: #fff;
+      font-size: 18px;
+    }
+
+    &__temp {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
+.temp {
+  &__info {
+    color: #fff;
+    font-size: 18px;
+  }
+}
+</style>
